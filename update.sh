f5ce3d0d80dfd04fe0e7d8e3dc8531923bd636c0
Ross Duggan <rduggan@engineyard.com>
2014-02-05 14:58:44 +0000

    Release upstream 0.7, packaging revision 0

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+##
+# A helper script for updates
+
+VERSION=0.7
+REVISION=0
+
+changelog=debian/changelog
+tempfile=$(mktemp -t debcontrol)
+
+rfc2822_date="$(date +"%a, %d %b %Y %T %z")"
+series=precise
+
+printf "Updating..."
+echo "memcached-top (${VERSION}-${REVISION}+${series}1) $series; urgency=low
+
+  * Upstream release ${VERSION}.
+
+ -- Ross Duggan <ross.duggan@acm.org>  $rfc2822_date
+" > $changelog
+
+git add *
+git commit -am "Release upstream $VERSION, packaging revision $REVISION" && \
+git tag -a $VERSION -m $VERSION
+printf "Ready to push.\n"
